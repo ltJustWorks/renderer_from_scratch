0bfae0fd23f4447da13d5cd596975c0278797daf
Lenovo-PC-2 <43526682+ltJustWorks@users.noreply.github.com>
2023-12-23 14:32:33 -0500

    render image buffer in window

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 use minifb::{Window, WindowOptions};
-use tgaimage::{TGAImage, TGAColor};
+use tgaimage::{TGAImage, TGAColor, TGAColorRGBA};
 
 fn convert_buf(image: &TGAImage) -> Vec<u32> {
     let mut buffer: Vec<u32> = Vec::new();
+
+    for y in 0..image.height() {
+        for x in 0..image.width() {
+            let rgba: TGAColorRGBA = match image.get(x, y) {
+                TGAColor::Rgb(rgb) => TGAColorRGBA{r: rgb.r, g: rgb.g, b: rgb.b, a: 255},
+                TGAColor::Rgba(rgba) => rgba,
+            };
+            let pixel_value = ((rgba.a as u32) << 24) | ((rgba.r as u32) << 16) | ((rgba.g as u32) << 8) | (rgba.b as u32);
+            buffer.push(pixel_value);
+        }
+    }
 
     buffer
 }
@@ -11,6 +22,7 @@ fn main() {
     let white = TGAColor::rgba(255, 255, 255, 255);
     let red = TGAColor::rgba(255, 0, 0, 255);
 
+    // TODO: Fix window w/h issue
     let width = 100;
     let height = 100;
 
@@ -19,7 +31,6 @@ fn main() {
     image.flip_vertically();
     image.write_tga_file("output.tga", false);
 
-    /*
     // Create a window matching the image dimensions
     let mut window = Window::new("View", width, height, WindowOptions::default()).expect("unable to open window");
 
@@ -29,5 +40,4 @@ fn main() {
     while window.is_open() {
         window.update_with_buffer(&buffer, width, height);
     }
-    */
 }
