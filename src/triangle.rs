af3af5cb3619f2c1b966543ffc1f6d453989fbbc
Lenovo-PC-2 <43526682+ltJustWorks@users.noreply.github.com>
2023-12-25 18:51:11 -0500

    full scanline drawing

--- src/triangle.rs
+++ src/triangle.rs
@@ -1,4 +1,4 @@
-use std::mem::swap;
+use std::{mem::swap, f32::consts::E};
 
 use tgaimage::{TGAImage, TGAColor};
 
@@ -15,15 +15,32 @@ pub fn draw(image: &mut TGAImage, color: &TGAColor, t0: &Point2D, t1: &Point2D, 
 
     let height = t_max.y - t_min.y;
 
-    for y in t_min.y..=t_mid.y {
-        let segment_height = t_mid.y - t_min.y + 1;
-        let a = (y - t_min.y) as f32 / height as f32;
-        let b = (y - t_min.y) as f32 / segment_height as f32;
+    for y in 0..height {
+        let in_second_half: bool = y > (t_mid.y - t_min.y);
+        let segment_height = if in_second_half {
+            t_max.y - t_mid.y
+        } else {
+            t_mid.y - t_min.y
+        };
 
-        let a_x = t_min.x + (a*((t_max.x - t_min.x) as f32)) as i32;
-        let b_x = t_min.x + (b*((t_mid.x - t_min.x) as f32)) as i32;
+        let a = y as f32 / height as f32;
+        let b = if in_second_half {
+            (y - (t_mid.y - t_min.y)) as f32 / segment_height as f32
+        } else {
+            y as f32 / segment_height as f32
+        } ;
 
-        image.set(a_x as usize, y as usize, color);
-        image.set(b_x as usize, y as usize, color);
+        let mut a_x = t_min.x + (a*((t_max.x - t_min.x) as f32)) as i32;
+        let mut b_x = if in_second_half {
+            t_mid.x + (b*((t_max.x - t_mid.x) as f32)) as i32
+        } else {
+            t_min.x + (b*((t_mid.x - t_min.x) as f32)) as i32
+        };
+
+        if a_x > b_x {std::mem::swap(&mut a_x, &mut b_x);}
+
+        for x in a_x..=b_x {
+            image.set(x as usize, (y + t_min.y) as usize, color);
+        }
     }
 }
